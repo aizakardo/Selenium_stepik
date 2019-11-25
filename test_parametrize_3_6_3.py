c2efd101ccb0f15779e951c7ad9d241f77635e07
William <49882511+aizakardo@users.noreply.github.com>
2019-11-25 18:36:57 +0300

    improve 3.6.3 test

--- test_parametrize_3_6_3.py
+++ test_parametrize_3_6_3.py
@@ -11,16 +11,18 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-@pytest.mark.parametrize('number', ["895", "896", "897", "898", "899", "903", "904", "905"])
+@pytest.mark.parametrize('number', ["895", "896", "897","898", "899", "903", "904", "905"]) #
 def test_guest_should_see_login_link(browser, number):
 #   link = f"http://selenium1py.pythonanywhere.com/{language}/"
     link = f"https://stepik.org/lesson/236{number}/step/1"
     browser.get(link)
     answer = math.log(int(time.time()))
+    browser.implicitly_wait(3)
     area = browser.find_element_by_css_selector(".textarea")
-    area.send_keys(answer)
-    browser.find_element_by_css_selector(".submit-submission")
-
-   
-
-answer = math.log(int(time.time()))
+    area.send_keys(str(answer))
+    button = browser.find_element_by_css_selector(".submit-submission")
+    button.click()
+    text = browser.find_element_by_css_selector(".smart-hints__hint")
+    correct_text = text.text
+    correct = "Correct!"
+    assert correct_text == correct, print(f" got {correct_text}")
--- test_test.py
+++ test_test.py
@@ -1,19 +1,28 @@
 import pytest
 import time
 import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 
 browser = webdriver.Chrome()
-number = '895'
-link = f"https://stepik.org/lesson/236{number}/step/1"
+number = '897'
+link = "https://stepik.org/lesson/236903/step/1"
 browser.get(link)
 
-#browser.implicitly_wait(3)
+browser.implicitly_wait(3)
+
+# area = WebDriverWait(browser, 5).until(
+#         EC.element_to_be_clickable((By.CSS_SELECTOR, ".textarea"))
 
 area = browser.find_element_by_css_selector(".textarea")
-answer = int(math.log(int(time.time())))
-area.send_keys(answer)
+answer = math.log(int(time.time()))
+area.send_keys(str(answer))
 button = browser.find_element_by_css_selector(".submit-submission")
 button.click()
 time.sleep(10)
 browser.quit()
+
+find_text = browser.find_element_by_css_selector(".smart-hints__hint")
+assert find_text.text == "Correct"
